85a2bc8065f021b012a03520bb56a2e9b7f237da
Jasper St. Pierre <jstpierre@mecheye.net>
2013-03-15 21:17:36 -0400

    Remove the demo animation button
    It's fairly outdated. It may come back one day in the frame.

--- demo.js
+++ demo.js
@@ -24,24 +24,6 @@
         },
     });
 
-    var SimpleButton = new Class({
-        Extends: Button,
-        initialize: function(standardColor, hoverColor) {
-            this.parent();
-            this._standardColor = standardColor;
-            this._hoverColor = hoverColor;
-        },
-        _syncButtonState: function() {
-            this.parent();
-            var color;
-            if (this._state == "up" || this._state == "down")
-                color = this._standardColor;
-            else if (this._state == "over")
-                color = this._hoverColor;
-            this._server.changeAttributes(this, this._windowId, { backgroundColor: color });
-        },
-    });
-
     var Launcher = new Class({
         Extends: Window,
         initialize: function(imageSrc, callback) {
@@ -129,27 +111,6 @@
     launcher.moveResize(10, 10, undefined, undefined);
     launcher.map();
 
-    function animWindow(window, freq, amplitude) {
-        var delay = 50;
-        var stepsPerSec = 1000 / delay;
-
-        var time = 0;
-        var coords = window.getRootCoords();
-        var origX = coords.x;
-
-        var step = freq * (Math.PI * 2 / stepsPerSec);
-
-        function animate() {
-            var offs = Math.sin(time) * amplitude;
-            var x = (origX + offs) | 0;
-            window.moveResize(x, undefined, undefined, undefined);
-            time += step;
-            return true;
-        }
-        var task = new Task(animate, delay);
-        return task;
-    }
-
     var cascade = 40;
     var windowNumber = 0;
 
@@ -161,25 +122,6 @@
         w.moveResize(windowNumber * cascade, windowNumber * cascade, undefined, undefined);
         w.changeProperty("WM_NAME", "Terminal Window " + windowNumber);
         w.map();
-
-        var button;
-
-        var buttonX = 10;
-        function setupButton(button) {
-            button.connect(server);
-            button.reparent(w);
-            button.moveResize(buttonX, 10, 20, 20);
-            button.map();
-            buttonX += 30;
-        }
-
-        var freq = (windowNumber - 1) * 0.25 + 0.5;
-        var animTask = animWindow(w, freq, 40);
-        button = new SimpleButton('#ffaa00', '#ffcc00');
-        setupButton(button);
-        button.clickCallback = function(event) {
-            animTask.toggle();
-        };
     }
 
     window.addEventListener("keydown", function(evt) {
