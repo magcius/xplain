26be6dce4fcfe7eb83086dc0056f613678305fd8
Jasper St. Pierre <jstpierre@mecheye.net>
2013-09-07 21:38:54 -0400

    Reindent class.js

--- src/class.js
+++ src/class.js
@@ -1,66 +1,66 @@
 (function(exports) {
-    "use strict";
+	"use strict";
 
-    function _parent() {
-	if (!this.__caller__)
-	    throw new TypeError("The method 'parent' cannot be called");
+	function _parent() {
+		if (!this.__caller__)
+			throw new TypeError("The method 'parent' cannot be called");
 
-	var caller = this.__caller__;
-	var name = caller._name;
-	var parent = caller._owner.__super__;
+		var caller = this.__caller__;
+		var name = caller._name;
+		var parent = caller._owner.__super__;
 
-	var previous = parent ? parent.prototype[name] : undefined;
+		var previous = parent ? parent.prototype[name] : undefined;
 
-	if (!previous)
-	    throw new TypeError("The method '" + name + "' is not on the superclass");
+		if (!previous)
+			throw new TypeError("The method '" + name + "' is not on the superclass");
 
-	return previous.apply(this, arguments);
-    }
-
-    function wrapFunction(obj, name, meth) {
-	if (meth._origin) meth = meth._origin;
-
-	function wrapper() {
-	    this.__caller__ = wrapper;
-	    var result = meth.apply(this, arguments);
-	    this.__caller__ = null;
-	    return result;
+		return previous.apply(this, arguments);
 	}
 
-	wrapper._origin = meth;
-	wrapper._name = name;
-	wrapper._owner = obj;
+	function wrapFunction(obj, name, meth) {
+		if (meth._origin) meth = meth._origin;
 
-	return wrapper;
-    }
+		function wrapper() {
+			this.__caller__ = wrapper;
+			var result = meth.apply(this, arguments);
+			this.__caller__ = null;
+			return result;
+		}
 
-    function Class(params) {
-	var newClass = function() {
-	    if (this.initialize)
-	        this.initialize.apply(this, arguments);
+		wrapper._origin = meth;
+		wrapper._name = name;
+		wrapper._owner = obj;
+
+		return wrapper;
+	}
+
+	function Class(params) {
+		var newClass = function() {
+			if (this.initialize)
+				this.initialize.apply(this, arguments);
+		};
+
+		var parent = params.Extends;
+		if (parent) {
+			newClass.__super__ = parent;
+			newClass.prototype = Object.create(parent.prototype);
+		}
+
+		for (var prop in params) {
+			var value = params[prop];
+
+			if (typeof value === 'function')
+				value = wrapFunction(newClass, prop, value);
+
+			newClass.prototype[prop] = value;
+		}
+
+		newClass.prototype.constructor = newClass;
+		newClass.prototype.parent = _parent;
+
+		return newClass;
 	};
 
-	var parent = params.Extends;
-	if (parent) {
-	    newClass.__super__ = parent;
-	    newClass.prototype = Object.create(parent.prototype);
-	}
-
-	for (var prop in params) {
-	    var value = params[prop];
-
-	    if (typeof value === 'function')
-	        value = wrapFunction(newClass, prop, value);
-
-	    newClass.prototype[prop] = value;
-	}
-
-	newClass.prototype.constructor = newClass;
-	newClass.prototype.parent = _parent;
-
-	return newClass;
-    };
-
-    exports.Class = Class;
+	exports.Class = Class;
 
 })(window);
