7535b76842e5a6569ccbb2493612daa34c06470c
Jasper St. Pierre <jstpierre@mecheye.net>
2013-09-08 20:51:27 -0400

    Hack on demos some more
    Remove toolkit use. This thing is helping us less and less.

--- article/demos.js
+++ article/demos.js
@@ -1,20 +1,6 @@
 (function(exports) {
     "use strict";
 
-    var ColoredWindow = new Class({
-        Extends: Window,
-        initialize: function(color) {
-            this.parent();
-            this._color = color;
-        },
-        connect: function(server) {
-            this.parent(server);
-            this._display.changeAttributes({ windowId: this.windowId,
-                                             backgroundColor: this._color });
-            this._display.mapWindow({ windowId: this.windowId });
-        },
-    });
-
     function makeStipple(display) {
         var stipple = display.createPixmap({ width: 2, height: 2 });
         display.drawTo(stipple, function(ctx) {
@@ -28,39 +14,47 @@
         return stipple;
     }
 
-    function makeDemoServer(elem) {
+    function bootstrapServer(elem) {
+        var width = elem.clientWidth;
+        var height = elem.clientHeight;
+
         var server = new Server();
-        server.resize(elem.clientWidth, elem.clientHeight);
+        server.resize(width, height);
+        elem.appendChild(server.elem);
+
         var connection = server.clientConnected(this);
         var display = connection.display;
         var stipple = makeStipple(display);
         display.changeAttributes({ windowId: display.rootWindowId,
                                    backgroundPixmap: stipple });
-        return server;
+
+        return { display: display, server: server };
     }
 
     function test(elem) {
-        var server = makeDemoServer(elem);
-        elem.appendChild(server.elem);
+        bootstrapServer(elem);
     }
 
     function demo1(elem) {
-        var server = makeDemoServer(elem);
-        elem.appendChild(server.elem);
+        var res = bootstrapServer(elem);
+        var display = res.display;
 
-        var width = elem.clientWidth;
-        var height = elem.clientHeight;
+        var rootGeom = display.getGeometry({ drawableId: display.rootWindowId });
 
         var spacing = 20;
         ['#ff0000', '#00ff00', '#0000ff'].forEach(function(color, i, arr) {
-            var w = new ColoredWindow(color);
-            w.width = 50;
-            w.height = 50;
+            var width = 50;
+            var height = 50;
 
-            var totalSize = (w.width + spacing) * arr.length - spacing;
-            w.x = (width - totalSize) / 2 + (i * (w.width + spacing));
-            w.y = (height - w.height) / 2;
-            w.connect(server);
+            var totalWidth = (width + spacing) * arr.length - spacing;
+
+            var x = (rootGeom.width - totalWidth) / 2 + (i * (width + spacing));
+            var y = (rootGeom.height - height) / 2;
+
+            var windowId = display.createWindow({ x: x, y: y, width: width, height: height });
+            display.changeAttributes({ windowId: windowId,
+                                       backgroundColor: color });
+            display.mapWindow({ windowId: windowId });
         });
     }
 
