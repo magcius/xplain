1f34f108f5888dd5cbfe781790f9a81f04d0ad9a
Jasper St. Pierre <jstpierre@mecheye.net>
2013-09-08 23:34:59 -0400

    Split out roundedRectRegion

--- src/clients/toolkit.js
+++ src/clients/toolkit.js
@@ -18,6 +18,51 @@
         image.src = src;
     };
 
+    Util.roundedRectRegion = function(geom, corners) {
+        var shapeRegion = new Region();
+        shapeRegion.init_rect(0, 0, geom.width, geom.height);
+
+        var cornerRegion = new Region();
+
+        function widthForRadiusSegment(radius, i) {
+            var r = radius-i-0.5;
+            return (0.5 + radius - Math.sqrt(radius*radius - r*r)) | 0;
+        }
+
+        if (corners.topRight) {
+            for (var i = 0; i < corners.topLeft; i++) {
+                var width = widthForRadiusSegment(corners.topLeft, i);
+                cornerRegion.union_rect(cornerRegion, 0, i, width, 1);
+            }
+        }
+
+        if (corners.topRight) {
+            for (var i = 0; i < corners.topRight; i++) {
+                var width = widthForRadiusSegment(corners.topRight, i);
+                cornerRegion.union_rect(cornerRegion, geom.width - width, i, width, 1);
+            }
+        }
+
+        if (corners.bottomLeft) {
+            for (var i = 0; i < corners.bottomLeft; i++) {
+                var width = widthForRadiusSegment(corners.bottomLeft, i);
+                cornerRegion.union_rect(cornerRegion, 0, geom.height - i - 1, width, 1);
+            }
+        }
+
+        if (corners.bottomRight) {
+            for (var i = 0; i < corners.bottomRight; i++) {
+                var width = widthForRadiusSegment(corners.bottomRight, i);
+                cornerRegion.union_rect(cornerRegion, geom.width - width, geom.height - i - 1, width, 1);
+            }
+        }
+
+        shapeRegion.subtract(shapeRegion, cornerRegion);
+        cornerRegion.finalize();
+
+        return shapeRegion;
+    };
+
     var Window = new Class({
         initialize: function() {
             this.x = 0;
--- src/wm/wm.js
+++ src/wm/wm.js
@@ -15,51 +15,6 @@
         return a;
     }
 
-    function roundedRectRegion(geom, corners) {
-        var shapeRegion = new Region();
-        shapeRegion.init_rect(0, 0, geom.width, geom.height);
-
-        var cornerRegion = new Region();
-
-        function widthForRadiusSegment(radius, i) {
-            var r = radius-i-0.5;
-            return (0.5 + radius - Math.sqrt(radius*radius - r*r)) | 0;
-        }
-
-        if (corners.topRight) {
-            for (var i = 0; i < corners.topLeft; i++) {
-                var width = widthForRadiusSegment(corners.topLeft, i);
-                cornerRegion.union_rect(cornerRegion, 0, i, width, 1);
-            }
-        }
-
-        if (corners.topRight) {
-            for (var i = 0; i < corners.topRight; i++) {
-                var width = widthForRadiusSegment(corners.topRight, i);
-                cornerRegion.union_rect(cornerRegion, geom.width - width, i, width, 1);
-            }
-        }
-
-        if (corners.bottomLeft) {
-            for (var i = 0; i < corners.bottomLeft; i++) {
-                var width = widthForRadiusSegment(corners.bottomLeft, i);
-                cornerRegion.union_rect(cornerRegion, 0, geom.height - i - 1, width, 1);
-            }
-        }
-
-        if (corners.bottomRight) {
-            for (var i = 0; i < corners.bottomRight; i++) {
-                var width = widthForRadiusSegment(corners.bottomRight, i);
-                cornerRegion.union_rect(cornerRegion, geom.width - width, geom.height - i - 1, width, 1);
-            }
-        }
-
-        shapeRegion.subtract(shapeRegion, cornerRegion);
-        cornerRegion.finalize();
-
-        return shapeRegion;
-    }
-
     // Don't extend Window as this needs to be in the
     // WM client, not its own client.
     var WindowFrame = new Class({
@@ -124,7 +79,7 @@
                 // Invalidate the frame that's already been partially painted.
                 this._display.invalidateWindow({ windowId: this._frameWindowId });
 
-                var shapeRegion = roundedRectRegion(this._frameGeometry, { topLeft: 10, topRight: 10 });
+                var shapeRegion = Util.roundedRectRegion(this._frameGeometry, { topLeft: 10, topRight: 10 });
                 this._display.setWindowShapeRegion({ windowId: this._frameWindowId,
                                                      shapeType: "Bounding",
                                                      region: shapeRegion });
@@ -141,7 +96,7 @@
             this._display.changeAttributes({ windowId: buttonWindowId, cursor: "pointer" });
             var radius = size / 2;
             var corners = { topLeft: radius, topRight: radius, bottomLeft: radius, bottomRight: radius };
-            var shapeRegion = roundedRectRegion(geom, corners);
+            var shapeRegion = Util.roundedRectRegion(geom, corners);
             this._display.setWindowShapeRegion({ windowId: buttonWindowId,
                                                  shapeType: "Bounding",
                                                  region: shapeRegion });
