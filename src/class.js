89920f2462a3037ca47dc78d893a84a3769b8063
Jasper St. Pierre <jstpierre@mecheye.net>
2013-12-20 16:40:09 -0500

    Tabs to spaces

--- src/class.js
+++ src/class.js
@@ -1,66 +1,66 @@
 (function(exports) {
-	"use strict";
+    "use strict";
 
-	function _parent() {
-		if (!this.__caller__)
-			throw new TypeError("The method 'parent' cannot be called");
+    function _parent() {
+        if (!this.__caller__)
+            throw new TypeError("The method 'parent' cannot be called");
 
-		var caller = this.__caller__;
-		var name = caller._name;
-		var parent = caller._owner.__super__;
+        var caller = this.__caller__;
+        var name = caller._name;
+        var parent = caller._owner.__super__;
 
-		var previous = parent ? parent.prototype[name] : undefined;
+        var previous = parent ? parent.prototype[name] : undefined;
 
-		if (!previous)
-			throw new TypeError("The method '" + name + "' is not on the superclass");
+        if (!previous)
+            throw new TypeError("The method '" + name + "' is not on the superclass");
 
-		return previous.apply(this, arguments);
-	}
+        return previous.apply(this, arguments);
+    }
 
-	function wrapFunction(obj, name, meth) {
-		if (meth._origin) meth = meth._origin;
+    function wrapFunction(obj, name, meth) {
+        if (meth._origin) meth = meth._origin;
 
-	    function wrapper() {
-	        this.__caller__ = wrapper;
-	        var result = meth.apply(this, arguments);
-	        this.__caller__ = null;
-	        return result;
-	    }
+        function wrapper() {
+            this.__caller__ = wrapper;
+            var result = meth.apply(this, arguments);
+            this.__caller__ = null;
+            return result;
+        }
 
-		wrapper._origin = meth;
-		wrapper._name = name;
-		wrapper._owner = obj;
+        wrapper._origin = meth;
+        wrapper._name = name;
+        wrapper._owner = obj;
 
-		return wrapper;
-	}
+        return wrapper;
+    }
 
-	function Class(params) {
-		var newClass = function() {
-			if (this.initialize)
-				this.initialize.apply(this, arguments);
-		};
+    function Class(params) {
+        var newClass = function() {
+            if (this.initialize)
+                this.initialize.apply(this, arguments);
+        };
 
-		var parent = params.Extends;
-		if (parent) {
-			newClass.__super__ = parent;
-			newClass.prototype = Object.create(parent.prototype);
-		}
+        var parent = params.Extends;
+        if (parent) {
+            newClass.__super__ = parent;
+            newClass.prototype = Object.create(parent.prototype);
+        }
 
-		for (var prop in params) {
-			var value = params[prop];
+        for (var prop in params) {
+            var value = params[prop];
 
-			if (typeof value === 'function')
-				value = wrapFunction(newClass, prop, value);
+            if (typeof value === 'function')
+                value = wrapFunction(newClass, prop, value);
 
-			newClass.prototype[prop] = value;
-		}
+            newClass.prototype[prop] = value;
+        }
 
-		newClass.prototype.constructor = newClass;
-		newClass.prototype.parent = _parent;
+        newClass.prototype.constructor = newClass;
+        newClass.prototype.parent = _parent;
 
-		return newClass;
-	};
+        return newClass;
+    };
 
-	exports.Class = Class;
+    exports.Class = Class;
 
 })(window);
