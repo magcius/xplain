a56cc8c700062dcc4a1d1142d86bb7fdac580e0e
Jasper St. Pierre <jstpierre@mecheye.net>
2013-12-21 16:51:08 -0500

    expose: Allow dragging the kitten
    And increase the sway underneath

--- src/article-demos/expose.js
+++ src/article-demos/expose.js
@@ -120,7 +120,7 @@
     var PERIOD = 6;
 
     // The number of pixels to sway to either side.
-    var SWAY_AMOUNT = 50;
+    var SWAY_AMOUNT = 75;
 
     // Shakes a window
     var WindowShaker = new Class({
@@ -154,6 +154,80 @@
         },
     });
 
+    var WindowDragger = new Class({
+        initialize: function(server, windowId) {
+            var connection = server.connect();
+            this._display = connection.display;
+            var port = connection.clientPort;
+            port.addEventListener("message", function(messageEvent) {
+                this._handleEvent(messageEvent.data);
+            }.bind(this));
+
+            this._windowId = windowId;
+
+            this._display.selectInput({ windowId: this._windowId,
+                                        events: ['ButtonPress'] });
+
+            this._rootMouseX = -1;
+            this._rootMouseY = -1;
+
+            this._winMouseX = -1;
+            this._winMouseY = -1;
+        },
+
+        // Make it move.
+        _sync: function() {
+            var newX = this._rootMouseX - this._winMouseX;
+            var newY = this._rootMouseY - this._winMouseY;
+            this._display.configureWindow({ windowId: this._windowId, x: newX, y: newY });
+        },
+
+        _updateWindowFromEvent: function(event) {
+            this._rootMouseX = event.rootX;
+            this._rootMouseY = event.rootY;
+            this._sync();
+        },
+
+        _handleButtonPress: function(event) {
+            // Only allow dragging with left-click
+            if (event.button != 1)
+                return;
+
+            this._winMouseX = event.winX;
+            this._winMouseY = event.winY;
+            this._updateWindowFromEvent(event);
+
+            this._display.grabPointer({ windowId: this._windowId,
+                                        ownerEvents: true,
+                                        events: ["ButtonRelease", "Motion"],
+                                        pointerMode: "Async",
+                                        cursor: "grabbing" });
+        },
+
+        _handleButtonRelease: function(event) {
+            if (event.button != 1)
+                return;
+
+            this._updateWindowFromEvent(event);
+            this._display.ungrabPointer({ windowId: this._windowId });
+        },
+
+        _handleMotion: function(event) {
+            this._updateWindowFromEvent(event);
+        },
+
+        _handleEvent: function(event) {
+            switch (event.type) {
+                case 'ButtonPress':
+                    return this._handleButtonPress(event);
+                case 'ButtonRelease':
+                    return this._handleButtonRelease(event);
+                case 'Motion':
+                    return this._handleMotion(event);
+            }
+        },
+    });
+
     ArticleDemos.registerDemo("expose", function(res) {
         var server = res.server;
         var display = res.display;
@@ -167,6 +241,8 @@
         var kitten1 = new SimpleImage(server, "kitten1.png");
         Util.centerWindow(display, kitten1.windowId);
         display.mapWindow({ windowId: kitten1.windowId });
+
+        var dragger = new WindowDragger(server, kitten1.windowId);
 
         var shaker = new WindowShaker(server, kitten2.windowId);
         shaker.start();
